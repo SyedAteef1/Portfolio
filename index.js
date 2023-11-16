9a44699f3770502ea6b6c6015e90154e3b83dffc
SyedAteef1 <vsyedateefquadri@gmail.com>
2023-11-16 19:30:46 +0530

    ninth commit

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const scroll = new LocomotiveScroll({
 //gsap animation
 gsap.from(".nlink" ,{
     stagger: .1,
-    y:10,
-    duration: 3,
+    y:-10,
+    duration: 2,
     ease: Power3,
+    opacity: 0
+})
+gsap.from("#hero" ,{
+    stagger: .1,
+    y:100,
+    duration: 2,
+    ease: Power2,
     opacity: 0
 })
